814dbef7e8eb9730f94294510d1629296b7b8b4c
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-06-22 18:43:22 +0200

    v1 - table of similarrity

--- app-backend/src/main/java/com/example/app_backend/similarity/BaseCbrApplication.java
+++ app-backend/src/main/java/com/example/app_backend/similarity/BaseCbrApplication.java
@@ -46,6 +46,39 @@ public class BaseCbrApplication implements StandardCBRApplication {
         simConfig = new NNConfig(); // KNN configuration
         simConfig.setDescriptionSimFunction(new Average()); // global similarity function = average
 
+        TabularSimilarity slicnostPrethodnoOsudjivan = new TabularSimilarity(Arrays.asList("da", "neosuđivan"));
+        slicnostPrethodnoOsudjivan.setSimilarity("da", "neosuđivan", 0.2);
+        simConfig.addMapping(new Attribute("previouslyConvicted", CaseDetails.class), slicnostPrethodnoOsudjivan);
+
+        TabularSimilarity slicnostZaIstoDelo = new TabularSimilarity(Arrays.asList("da", "ne", "neosuđivan"));
+        slicnostZaIstoDelo.setSimilarity("da", "ne", 0.3);
+        slicnostZaIstoDelo.setSimilarity("da", "neosuđivan", 0.2);
+        slicnostZaIstoDelo.setSimilarity("ne", "neosuđivan", 0.6);
+        simConfig.addMapping(new Attribute("forSameOffense", CaseDetails.class), slicnostZaIstoDelo);
+
+        TabularSimilarity slicnostIlegalnoOruzje = new TabularSimilarity(Arrays.asList("da", "ne"));
+        slicnostIlegalnoOruzje.setSimilarity("da", "ne", 0.1);
+        simConfig.addMapping(new Attribute("illegallyPossessesWeapon", CaseDetails.class), slicnostIlegalnoOruzje);
+
+        TabularSimilarity slicnostPriznaoKrivicu = new TabularSimilarity(Arrays.asList("da", "ne"));
+        slicnostPriznaoKrivicu.setSimilarity("da", "ne", 0.2);
+        simConfig.addMapping(new Attribute("admittedGuilt", CaseDetails.class), slicnostPriznaoKrivicu);
+
+        TabularSimilarity slicnostKajanje = new TabularSimilarity(Arrays.asList("da", "ne"));
+        slicnostKajanje.setSimilarity("da", "ne", 0.3);
+        simConfig.addMapping(new Attribute("remorseful", CaseDetails.class), slicnostKajanje);
+
+        TabularSimilarity slicnostImovnoStanje = new TabularSimilarity(
+                Arrays.asList("dobrog imovnog stanja", "srednjeg imovnog stanja", "lošeg imovnog stanja"));
+        slicnostImovnoStanje.setSimilarity("dobrog imovnog stanja", "srednjeg imovnog stanja", 0.7);
+        slicnostImovnoStanje.setSimilarity("dobrog imovnog stanja", "lošeg imovnog stanja", 0.3);
+        slicnostImovnoStanje.setSimilarity("srednjeg imovnog stanja", "lošeg imovnog stanja", 0.6);
+        simConfig.addMapping(new Attribute("financialStatus", CaseDetails.class), slicnostImovnoStanje);
+
+        simConfig.addMapping(new Attribute("weaponType", CaseDetails.class),
+                new es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.local.MaxString());
+        simConfig.addMapping(new Attribute("ammunitionCount", CaseDetails.class), new Interval(50));
+
         // Equal - returns 1 if both individuals are equal, otherwise returns 0
         // Interval - returns the similarity of two number inside an interval: sim(x,y)
         // = 1-(|x-y|/interval)
